5d87b0a47caf2dc0d845b8de1202ff9e0101e06e
prashantsharma97 <prashantatul565@gmail.com>
2025-04-10 17:44:56 +0530

    changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const path = require('path');
 const fs = require('fs');
+const cors = require('cors');
 require('dotenv').config(); // Make sure you have a .env file
 
 const app = express();
